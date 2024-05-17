e7c03913e885cdd3afccaf98a9cf101a531c743c
Erik Isberg <erik@isberg.nu>
2024-05-17 14:37:49 +0200

    v1

--- components/button-scroll-to-bottom.tsx
+++ components/button-scroll-to-bottom.tsx
@@ -28,7 +28,7 @@ export function ButtonScrollToBottom({ className, ...props }: ButtonProps) {
       {...props}
     >
       <IconArrowDown />
-      <span className="sr-only">Scroll to bottom</span>
+      <span className="sr-only">Scrolla längst ner</span>
     </Button>
   )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -12,9 +12,8 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://supabase.com">Supabase</ExternalLink>.
+      Open source AI chatbot byggt av{' '}
+      <ExternalLink href="https://adeprimo.com">Adeprimo</ExternalLink>
     </p>
   )
 }
